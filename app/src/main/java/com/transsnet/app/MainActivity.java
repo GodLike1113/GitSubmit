34c23f3a62bcfc608ba2330148de2a46e9a3ee33
windyZeng <feng.zeng@transsnet.com>
2020-05-11 19:58:45 +0800

    创建了一个feature

--- app/src/main/java/com/transsnet/app/MainActivity.java
+++ app/src/main/java/com/transsnet/app/MainActivity.java
@@ -52,4 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void makeFeature(){
+        System.out.println("make feature v1.0");
+    }
 }
